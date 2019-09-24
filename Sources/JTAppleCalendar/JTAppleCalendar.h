074f0a3af7af6cb79b6d93a8da5cc4117f830652
Arnaud Boudou <boudouarnaud@gmail.com>
2019-09-24 13:48:20 +0200

    Carthage support.

---
+++ Sources/JTAppleCalendar/JTAppleCalendar.h
@@ -0,0 +1,19 @@
+//
+//  JTAppleCalendar.h
+//  JTAppleCalendar
+//
+//  Created by Arnaud Boudou on 24/09/2019.
+//  Copyright © 2019 Arnaud Boudou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JTAppleCalendar.
+FOUNDATION_EXPORT double JTAppleCalendarVersionNumber;
+
+//! Project version string for JTAppleCalendar.
+FOUNDATION_EXPORT const unsigned char JTAppleCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTAppleCalendar/PublicHeader.h>
+
+
